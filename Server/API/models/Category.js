5eb1cb5f82628561234605d7690580153fb1c704
celbax95 <loicmace95@gmail.com>
2020-04-20 10:58:24 +0200

    [BACKEND] Ajout du model et du controller pour les categories

--- Server/API/models/Category.js
+++ Server/API/models/Category.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
-    label: {
-        type: String, required: true,  unique: true
-    },
-    description: {
-        type: String, required: true
-    },
+  label: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('User', categorySchema);
+module.exports = mongoose.model("Category", categorySchema);
