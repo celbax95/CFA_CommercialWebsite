517af8293b2a6395c6d9952547d136fd454713b8
Aristide Bamenou <aristide.bamenou@hotmail.com>
2020-04-20 14:02:53 +0200

    Item and category done! Without auth.

--- Server/API/models/Order.js
+++ Server/API/models/Order.js
@@ -2,26 +2,9 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var OrderSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  buyDate: {
-    type: Date,
-    default: Date.now,
+  item: {
+    type: Schema.Types.ObjectId,
+    ref: 'Item',
     required: true,
   },
   seller: {
