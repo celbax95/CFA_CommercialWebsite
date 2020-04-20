114fcdec4a693ca88cad7100805bebc3aeea6a69
celbax95 <loicmace95@gmail.com>
2020-04-20 11:02:48 +0200

    [BACKEND] Ajout des attributs required dans les champs d'Order

--- Server/API/models/Order.js
+++ Server/API/models/Order.js
@@ -2,32 +2,36 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var OrderSchema = new Schema({
-  IdOrder: {
-    type: Number,
-  },
   title: {
     type: String,
+    required: true,
   },
   description: {
     type: String,
+    required: true,
   },
   price: {
     type: Number,
+    required: true,
   },
   image: {
     type: Image,
+    required: true,
   },
   date: {
     type: Date,
     default: Date.now,
+    required: true,
   },
   seller: {
     type: Schema.Types.ObjectId,
     ref: "User",
+    required: true,
   },
   buyer: {
     type: Schema.Types.ObjectId,
     ref: "User",
+    required: true,
   },
 });
 module.exports = mongoose.model("Order", OrderSchema);
