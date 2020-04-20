6607f5981a671b7e04a378ab7688e19675bd0508
celbax95 <loicmace95@gmail.com>
2020-04-20 17:16:38 +0200

    [BACKEND] Desactivation de la necessite d'authentification pour le DEV

--- Server/index.js
+++ Server/index.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-let useAuthentificate = true;
+let useAuthentificate = false;
 
 // Authentification
 const auth = require("./service/AuthService"); //importing route
